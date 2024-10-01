73eb3e5925232ac35607819b8b694f1767dab80e
BrandonKambasha <btkambasha@gmail.com>
2024-10-02 00:33:43 +0100

    remove hero image from index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,7 @@ export default function Home() {
       <main>
         <section className="relative py-20 overflow-hidden">
           <div className="absolute inset-0 bg-gradient-to-r from-orange-400 to-blue-500 opacity-80"></div>
-          <Image
-            src={myImage}
-            alt=""
-            layout="fill"
-            objectFit="cover"
-            className="mix-blend-overlay"
-            
-          />
+          
           <div className="container mx-auto px-4 text-center relative z-10">
             <h1 className="text-5xl font-bold mb-4 text-white drop-shadow-lg">Welcome to REG 44 CONSULTANCY</h1>
             <p className="text-xl max-w-2xl mx-auto mb-8 text-white drop-shadow-md">Pioneering excellence in Semi Independent Accommodation and Residential Children's Home care since 2010</p>
